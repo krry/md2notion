08c505e0772dd55cdebe532b06fcc7f34a57efa9
Kerry Snyder <kerry@atmanaut.me>
2022-12-21 01:30:40 -0800

    chore: add readme and docs

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# md2notion
+
+Import markdown into Notion on the command line
+
+Uses [martian](https://github.com/tryfabric/martian)
+
+```sh
+md2notion <cmd> [args]
+Imports markdown into a Notion database
+
+Options:
+      --version   Show version number                                  [boolean]
+  -c, --category  Category of the note imported       [string] [default: "note"]
+      --help      Show help                                            [boolean]
+```
--- bin/index.js
+++ bin/index.js
@@ -9,39 +9,18 @@ yargs(hideBin(process.argv))
   .scriptName("md2notion")
   .usage(usage)
   .command(
-    "rake [dir]",
-    "imports all markdown files in top-level of this directory",
-    (yargs) => {
-      yargs.positional("dir", {
-        type: "string",
-        default: ".",
-        describe: "from whence the markdown files come",
-      });
-    },
-    function (argv) {
-      importMarkdownFromDir({ dir: argv.dir, category: argv.category });
-    }
-  )
-  .command(
     "$0",
-    "imports a single markdown file",
+    "imports a markdown file or directory of markdown recursively",
     () => {},
     function (argv) {
       importMarkdownFile({ file: argv._[0], category: argv.category });
     }
   )
   .option("c", {
-    alias: ["cat", "category"],
+    alias: ["category"],
     default: "note",
     demandOption: false,
     describe: "Category of the note imported",
-    type: "string",
-  })
-  .option("d", {
-    alias: ["dir", "directory"],
-    default: ".",
-    demandOption: false,
-    describe: "Directory to import from",
     type: "string",
   })
   .help(true).argv;
